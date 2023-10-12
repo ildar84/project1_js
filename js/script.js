3207ede561bf15c3bd26a3c487a5cc80db397b32
ildar <example@gmail.com>
2023-10-12 20:40:34 +0300

    12102023

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 "use strict";
 
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");   
+    }
+}
+
+start();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -11,13 +21,41 @@ let personalMovieDB = {
     privat: false
 }
 
-let a = prompt("Один из последних просмотренных фильмов ?", ""),
-    b = prompt("На сколько оцените его ?", ""),
-    c = prompt("Один из последних просмотренных фильмов ?", ""),
-    d = prompt("На сколько оцените его ?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+function rememberMyFilms() {
+    for (let i = 1; i < 3; i++) {
+        let a = prompt("Один из последних просмотренных фильмов ?", ""),
+            b = prompt("На сколько оцените его ?", "");
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log("ok");
+        } else {
+            i--;
+            console.log("error");
+        }    
+    }
+}
+
+rememberMyFilms();
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        alert("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
+
 
 console.log(personalMovieDB);
 
--- js/script_temp.js
+++ js/script_temp.js
@@ -26,10 +26,60 @@
 //     }
 // }
 
-const arrayOfNumbers = [];
+// const arrayOfNumbers = [];
 
-for (let i = 5; i <= 10; i++) {
-    arrayOfNumbers[i - 5] = i;
+// for (let i = 5; i <= 10; i++) {
+//     arrayOfNumbers[i - 5] = i;
+// }
+
+// console.log(arrayOfNumbers);
+
+// function sayHello(name) {
+//     let str = `Привет, ${name}`;
+//     return str;
+// }
+
+// console.log(sayHello('Антон'));
+
+// function getMathResult(num, pr) {
+//     if (pr > 0 && typeof(pr) === 'number') {
+//         let str = '';
+//         for (let i = 1; i <= pr; i++) {
+//             if (i === pr) {
+//                 str +=`${num * i}`;
+//             } else {
+//                 str +=`${num * i}---`
+//             }
+//         }
+
+//         return str;
+
+//     } else {
+//         return num;
+//     }
+
+// }
+
+// console.log(getMathResult(5, 3));
+// console.log(getMathResult(5, 0));
+
+function getMathResult(num, pr) {
+    if (pr > 0 && typeof(pr) === 'number') {
+        let str = '';
+        for (let i = 1; i <= pr; i++) {
+           
+            if (i === pr) {
+                str +=`${num * i}`;
+            } else {
+                str +=`${num * i}---`;
+            }
+               
+        }
+        return str;      
+    } else {
+        return num;
+    }
 }
 
-console.log(arrayOfNumbers);
+console.log(getMathResult(5, 3));
+console.log(getMathResult(5, 0));
