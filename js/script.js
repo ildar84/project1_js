1b7500771d64c73b79317a14b16d4f9626838899
ildar <example@gmail.com>
2023-11-27 15:45:34 +0300

    27112023

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");   
+    }
+}
+
+// start();
+
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +21,51 @@ let personalMovieDB = {
     privat: false
 };
 
-let a = prompt("Один из просмотренных фильмов", ""),
-    b = prompt("На сколько оцените его", ""),
-    c = prompt("Один из просмотренных фильмов", ""),
-    d = prompt("На сколько оцените его", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i ++) {
+        let a = prompt("Один из просмотренных фильмов", ""),
+            b = prompt("На сколько оцените его", "");
+            if ( a != null && b != null && a != '' &&  b != '' && a.length < 50 ) {
+                personalMovieDB.movies[a] = b;
+                } else {
+                    i --;
+                }
+    }
+}
+
+// rememberMyFilms();
+
+
+    
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 &&            personalMovieDB.count < 30) {
+            console.log("Вы классический зритель"); 
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB)
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++){
+        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
+    }
+}
+    
+writeYourGenres();
 console.log(personalMovieDB);
--- js/script_temp.js
+++ js/script_temp.js
@@ -1,164 +1,90 @@
-// for (let i = 5; i < 11; i++) {
-//     console.log(i);
-// }
+"use strict";
 
-// for (let i = 20; i > 10; i--) {
-//     if (i === 13)  break;
-//         console.log(i);
-    
-// }
+for (let i = 5; i < 11; i++) {
 
-// for (let i = 2; i <= 10; i++) {
-//     if ( i % 2 === 0) {
-//         console.log(i);
-//     }
-// }
+    console.log(i);
+}
 
-// let i = 2;
-// while (i <= 16) {
-//     if (i % 2 === 0) {
-//         i ++;
-//         continue;
+for (let i = 20; i >= 10; i--) {
+     
+    if (i == 13) {
+        break;
+    } else {
+        console.log(i); 
+    }
+}
+
+for (let i = 2; i < 11; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+let i = 2;
+
+while ( i <= 15) {
+    i++;
+
+    if (i % 2 === 0) {
+        continue;
         
-//     } else {
-//         console.log(i);
-//         i ++;
-//     }
-// }
+    } else {
+        console.log(i);
+        
+    }
+    
+    
+}
 
-// const arrayOfNumbers = [];
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
 
-// for (let i = 5; i <= 10; i++) {
-//     arrayOfNumbers[i - 5] = i;
-// }
+let arr = [];
+for (let i = 5; i < 11; i++) {
+    arr[i - 5] = i;
+}
 
-// console.log(arrayOfNumbers);
+console.log(arr);
 
-// function sayHello(name) {
-//     let str = `Привет, ${name}`;
-//     return str;
-// }
+function sayHello(name) {
+    let str = '';
+    str = `Привет,${name}`;
+    console.log(str);
+    return str;
+}
 
-// console.log(sayHello('Антон'));
+sayHello('Антон');
 
-// function getMathResult(num, pr) {
-//     if (pr > 0 && typeof(pr) === 'number') {
-//         let str = '';
-//         for (let i = 1; i <= pr; i++) {
-//             if (i === pr) {
-//                 str +=`${num * i}`;
-//             } else {
-//                 str +=`${num * i}---`
-//             }
-//         }
+function returnNeighboringNumbers(num) {
+    let arr = [];
+    arr = [num - 1, num, num + 1];
+    console.log(arr);
+}
 
-//         return str;
+returnNeighboringNumbers(5);
 
-//     } else {
-//         return num;
-//     }
+function getMathResult(num, p) {
+    if (typeof(p) !== 'number' || p <= 0) {
+        return num;
+    }
 
-// }
+    let str = '';
+    for (let i = 1; i <= p; i++) {
+        if (i === p) {
+            str += `${num * i}`;
+        } else {
+            str += `${num * i}---`
+        }
+    }
+    
+    console.log(str);
+    return str;
+}
 
-// console.log(getMathResult(5, 3));
-// console.log(getMathResult(5, 0));
-
-// function getMathResult(num, pr) {
-//     if (pr > 0 && typeof(pr) === 'number') {
-//         let str = '';
-//         for (let i = 1; i <= pr; i++) {
-           
-//             if (i === pr) {
-//                 str +=`${num * i}`;
-//             } else {
-//                 str +=`${num * i}---`;
-//             }
-               
-//         }
-//         return str;      
-//     } else {
-//         return num;
-//     }
-// }
-
-// console.log(getMathResult(5, 3));
-// console.log(getMathResult(5, 0));
-
-// const personalPlanPeter = {
-//     name: "Peter",
-//     age: "29",
-//     skills: {
-//         languages: ['ru', 'eng'],
-//         programmingLangs: {
-//             js: '20%',
-//             php: '10%'
-//         },
-//         exp: '1 month'
-//     },
-
-//     showAgeAndLangs: function(plan) {
-//         const {age} = plan;
-//         const {languages} = plan.skills;
-//         let str = `Мне ${age} и я владею языками: `;
-
-//         languages.forEach(function(lang) {
-//             str += `${lang.toUpperCase()} `;
-            
-//         });
-
-//         return str;
-//     }
-// };
-
-// console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
-
-
-
-
-// function showProgrammingLangs(plan) {
-//     let str = '';
-//     const {programmingLangs} = plan.skills;
-//     for (let key in programmingLangs) {
-//         str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
-//     }
-
-//     return str;
-// }
-// console.log(
-// showProgrammingLangs(personalPlanPeter));
-
-// function showExperience(plan) {
-
-
-//     // const {exp} = plan.skills;
-//     // return exp;
-
-// }
-
-// showExperience(personalPlanPeter);
-
-// const family = ['Peter', 'Ann', 'Alex', 'Linda'];
-
-// function showFamily(arr) {
-//     str = '';
-
-//     arr.length === 0 ? str = `Семья пуста` : str = `Семья состоит из `;
-//     arr.forEach(name => {
-//         str += `${name} `;
-//     });
-//     return str;    
-// }
-
-// console.log(showFamily(family));
-
-// const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
-
-// function standardizeStrings(arr) {
-//     arr.forEach(city => {
-//         console.log(city.toLowerCase());
-//     });
-// }
-
-// standardizeStrings(favoriteCities);
-
+getMathResult(5, 3);
 
